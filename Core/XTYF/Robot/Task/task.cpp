db70e06e89c660146af3b4082c3fea3f6ba3ae77
NUAAwyx <wangyingxv@nuaa.edu.cn>
2025-04-24 21:44:34 +0800

    2025.4.24-添加了VOFA相关代码

--- Core/XTYF/Robot/Task/task.cpp
+++ Core/XTYF/Robot/Task/task.cpp
@@ -6,14 +6,10 @@ bool init_finished;
 
 void Task_Init()
 {
-    FDCAN_Init(&hfdcan1, nullptr);
 
-    init_finished = true;
 }
 
 void Task()
 {
-    FDCAN1_0x200_Tx_Data[0] = 10;
-    FDCAN1_0x200_Tx_Data[1] = 10;
-    FDCAN_Send_Data(&hfdcan1, 0x200, FDCAN1_0x200_Tx_Data, 8);
+
 }
